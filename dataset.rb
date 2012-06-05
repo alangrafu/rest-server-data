9a1e5845bdadab3e787b52b6770d2942692452ad
alvaro <alvaro@alia.(none)>
2012-06-05 13:42:58 -0700

    Stores file using threadsx

--- dataset.rb
+++ dataset.rb
@@ -1,16 +1,55 @@
-require "sinatra"
-require "sinatra/reloader"
+require 'sinatra'
+require 'sinatra/reloader'
 require 'digest/md5'
 require 'json'
+require 'net/http'
+require 'uri'
 
 
-get '/dataset/*' do
-  dataset = params[:splat].first.to_s().gsub /^http:\//, 'http://' # => ["path/to/file", "xml"]
-  puts "Dataset => #{dataset}"
+
+def fetch(uri_str, directory, limit = 10)
+  # You should choose better exception.
+  raise ArgumentError, 'HTTP redirect too deep' if limit == 0
+  
+  url = URI.parse(uri_str)
+  req = Net::HTTP::Get.new(url.path, { 'User-Agent' => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_4) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.53 Safari/536.5" })
+  response = Net::HTTP.start(url.host, url.port) { |http| http.request(req) }
+  puts response
+  case response
+  when Net::HTTPSuccess     then 
+    puts "Downloaded!"
+    filename = directory+"/dataset"
+    open(filename, "wb") { |file|
+      file.write(response.body)
+    }
+  when Net::HTTPRedirection then 
+    puts "Redirected to "+response['location']
+    fetch(response['location'], directory, limit - 1)
+  else
+    response.error!
+  end
+end
+
+get '/dataset/*' do |p|
+  dataset = 'http://'+p.to_s()
   digest = Digest::MD5.hexdigest(dataset)
-  file = "tmp/#{digest}"
-  Dir.mkdir(file) unless File::directory?( file )
+  directory = "tmp/#{digest}"
   content_type 'json'
-  "hello world => #{digest}"
+  ""+directory
+  begin
+    threads = Thread.new(dataset, directory) {
+      Dir.mkdir(directory) unless File::directory?( directory )
+      puts "Downloading "+dataset
+      fetch(dataset, directory)
+    }
+  rescue
+    puts e.message  
+    puts e.backtrace.inspect 
+  end
+end
+
+
+get '/download/*' do |path|
+  [path] # => ["path/to/file", "xml"]
 end
 
